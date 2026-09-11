dbe855384e436cd85f5fc37a9b7abf79960f6b6c
Ray-Shu <rayshu1015@gmail.com>
2026-09-10 19:07:24 -0600

    merged with sheldon

--- a1.py
+++ a1.py
@@ -55,15 +55,6 @@ class CommandInterface:
             0: command failed 
         """
 
-        '''
-        check:
-        1. numheaps between 1 n 10 
-        2. each heap contains between 1 n 10 tokens
-        3. each token is has either color "b" or "w" and value between 1 n 20 (inclusive)
-        4. komi is an INT + 0.5. Komi in (-100, 100)
-        5. Black score = 0, White score = komi
-        '''
-
         # check if gamestate exists
         if "[" not in args:
             return 0
@@ -124,7 +115,7 @@ class CommandInterface:
             print(f"k {self.komi} {self.game_state}")
             return 1
         except: 
-            return -1
+            return 0
         
     def cmd_toplay(self, args: str) -> bool:
         if (args == "b" or args == "w"):
@@ -161,14 +152,43 @@ class CommandInterface:
             return -1
         
     def cmd_legal(self, args: str) -> bool:
-        return not_yet()
+        try:
+            heap = int(args)
+        except:
+            return -1
+        if heap < 0:
+            return - 1
+        if heap < len(self.game_state) and len(self.game_state[heap]) > 0: # 
+            print("yes")
+        else:
+            print("no")
+        return 1
+
     def cmd_genmove(self, args: str) -> bool:
-        return not_yet()
+        legal_heaps = []
+        for i in range(len(self.game_state)):
+            if len(self.game_state[i]) > 0:
+                legal_heaps.append(i)
+        if not legal_heaps:
+            return -1
+        heap = random.choice(legal_heaps)
+        self.cmd_play(str(heap))
+        print(heap)  
+        return 1
+
     def cmd_score(self, args: str) -> bool:
         print(f"b {self.black_score} w {self.white_score}")
         return 1
+
     def cmd_winner(self, args: str) -> bool:
-        return not_yet()
+        if any(len(heap) > 0 for heap in self.game_state): # game not finished, return -1
+            return - 1
+        if self.black_score > self.white_score:
+            print("b")
+        else:
+            print("w")
+        return 1
+        
 #============================================================================
 # End of functions requiring implementation
 #============================================================================
